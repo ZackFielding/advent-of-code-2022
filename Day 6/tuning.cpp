6da362f1f64df80b5e128719ebd0099915e0e6eb
Zack Fielding <zackfielding@gmail.com>
2022-12-08 14:42:52 -0700

    part 2 solution

--- Day 6/tuning.cpp
+++ Day 6/tuning.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <map>
 #include "tuning_funcs.hpp"
 
 int main(){
-
     std::ifstream buffer_file;
     buffer_file.open("buffer.txt");
 
@@ -14,9 +12,10 @@ int main(){
         readFile(buffer_file, str); 
         buffer_file.close();
 
-         // moving window to find first unique 4 char sequence position
          // part 1 solution
-        std::cout << "Position: " << findUnique4Char(&str) << '\n'; 
+        std::cout << "Start-of-packet marker position: " << findUniqueCharPattern<4>(&str) << '\n'; 
+         // part 2 solution
+        std::cout << "Start-of-message marker position: " << findUniqueCharPattern<14>(&str) << '\n';
     }
     return 0;
 }
--- Day 6/tuning_funcs.hpp
+++ Day 6/tuning_funcs.hpp
@@ -6,24 +6,28 @@
 
 void readFile(std::ifstream& buffer_file, std::string& str){
 
+     // read entire file into string stream
+     // NOTE: need to read into char buffer first or else reads as large int
     std::stringstream ss;
     char hold {};
-     // read entire file into string stream
-    while (!!buffer_file){
-        if (buffer_file.get(hold)) ss << hold;
-    }
+    while (!!buffer_file) if (buffer_file.get(hold)) ss << hold;
     ss >> str;
 }
 
-unsigned findUnique4Char(const std::string* str_v){
+// tempated to allow use for both solutions
+// moving window (size based on template) that uses map to find unique sequences 
+template <unsigned PATTERN_SIZE>
+unsigned findUniqueCharPattern(const std::string* str_v){
 
     std::map<char, bool> c_map;
-    for (unsigned start_idx {0}; start_idx < str_v->size()-4; ++start_idx){
-       for (unsigned current_idx {0}; current_idx < 4; ++current_idx){
+    for (unsigned start_idx {0}; start_idx < str_v->size()-PATTERN_SIZE; ++start_idx){
+       for (unsigned current_idx {0}; current_idx < PATTERN_SIZE; ++current_idx){
+         // will emplace new pair in map IF provided key is not already within map
             c_map.try_emplace(str_v->at(start_idx+current_idx), true);
-            if (c_map.size() == 4) return (start_idx + 1 + current_idx);
+        // once size reaches desired pattern size -> return current position in string
+            if (c_map.size() == PATTERN_SIZE) return (start_idx + 1 + current_idx);
        } 
-        c_map.clear();
+        c_map.clear(); // if pattern not found in window -> clear map to reuse next iteration
     }
     return 0; // error
 }
