f175db45924f3fa4b6d32f951dd105435a7e7dee
Zack Fielding <zackfielding@gmail.com>
2022-12-13 17:33:07 -0700

    part 2 solution

--- Day 9/rope_bridge.cpp
+++ Day 9/rope_bridge.cpp
@@ -12,9 +12,12 @@ int main(){
         std::vector<COORD> path;
         readFile(file, path);
 
-        // part 1 solution
-        std::cout << "Number of new blocks visited by rope tail: " 
-            << trackDisplacement(path) << '\n';
+        // part 1 & 2 solutions
+        std::pair<size_t, size_t> tail_visits {trackDisplacement(path)};
+        std::cout << "Number of new blocks visited by rope tail 1: " 
+            << tail_visits.first 
+            << "\nNumber of new blocks visited by rope tail 9: " 
+            << tail_visits.second << '\n';
     }
 
     return 0;
--- Day 9/rope_bridge_funcs.hpp
+++ Day 9/rope_bridge_funcs.hpp
@@ -8,6 +8,7 @@
 #include <map>
 #include <cmath>
 #include <cstring>
+#include <algorithm>
 
  // double to allow cmath funcs
 using COORD = std::pair<char, double>;
@@ -22,52 +23,81 @@ void readFile(std::ifstream& file, std::vector<COORD>& path){
     }
 }
 
-size_t trackDisplacement(std::vector<COORD>& path){
+double findResultant(double* head, double* tail, std::pair<double,double> offset = {0,0}){
+    return std::sqrt(
+                    std::pow((head[0]-(tail[0]+offset.first)), 2) 
+                    + std::pow((head[1]-(tail[1]+offset.second)), 2)); 
+}
 
-    std::map<std::pair<double,double>, bool> visited_map {{{0.0,0.0}, true}}; // vistied first coord (0,0)
-    double head[] {0,0}, tail[] {0,0}, *h_ptr {nullptr};
-    double prev_head[] {0,0};
-    double resultant {0};
-    double x_dif {0}, y_dif {0};
+std::pair<size_t, size_t> trackDisplacement(std::vector<COORD>& path){
+    std::map<std::pair<double,double>, bool> knot1_visited_map {{{0.0,0.0}, true}}; // vistied first coord (0,0)
+    std::map<std::pair<double,double>, bool> knot9_visited_map {{{0.0,0.0}, true}}; // vistied first coord (0,0)
+    std::vector<std::pair<double, double>> xy_displacements {{1,1}, {1,-1}, {-1,1}, {-1,-1}};
+    double *h_ptr {nullptr}, resultant {0}, x_dif {0}, y_dif {0};
+
+     // idx == 0 represents head
+    std::vector<double[2]> knots_vec (10); 
+     // set all double* to 0
+    std::for_each(knots_vec.begin(), knots_vec.end(), [](double* xy) 
+            { xy[0] = 0.0;
+              xy[1] = 0.0; });
 
     for (const auto& move : path){
-        h_ptr = (move.first == 'U' || move.first == 'D') ? &head[1] : &head[0];
+        h_ptr = (move.first == 'U' || move.first == 'D') ? &(knots_vec.at(0)[1]) : &(knots_vec.at(0)[0]);
         //std::clog << "\n==== " << move.first << ' ' << move.second << " ====\n";
         for (int disp {0}; disp < std::abs(move.second); ++disp){
-            std::memcpy(prev_head, head, sizeof head); // cache prev head before incrementing
-        
-        // up & right are positive polarities (increment) - down & left are negative
-            if (move.first == 'U' || move.first == 'R') ++(*h_ptr);
-            else --(*h_ptr);
+            for (size_t knot {0}; knot < knots_vec.size()-1; ++knot){
+            // direct changes to head ptr only occurs to HEAD .. so only apply this once for each iteration of this loop
+                if (knot == 0){
+                // up & right are positive polarities (increment) - down & left are negative
+                    if (move.first == 'U' || move.first == 'R') ++(*h_ptr);
+                    else --(*h_ptr);
+                }
 
-        // find new vec between head&tail
-            resultant = 
-                std::sqrt(
-                    std::pow((head[0]-tail[0]), 2) 
-                    + std::pow((head[1]-tail[1]), 2)); 
-        // if vec between head & tail > 1 .. need to change tail position
-            if (resultant > 1.5){
-               x_dif = std::abs(head[0] - tail[0]); // x&y vec between points 
-               y_dif = std::abs(head[1] - tail[1]);
-            
-            // if diagonal -> set tail to cached head && try insert
-               if (x_dif >= 1 && y_dif >= 1){
-                std::memcpy(tail, prev_head, sizeof prev_head);
-            
-            // x OR y vec == 0 -> whatever != 0, apply same move as applied to head
-               } else {
-                    (x_dif > 0) ? 
-                            tail[0] += (move.second / std::abs(move.second)) 
-                            : tail[1] += (move.second / std::abs(move.second));
-               }
-            
-             // after changing tail, try to insert new tail xy coord into map
-            visited_map.insert({{tail[0],tail[1]},true}); 
+                // find new vec between head&tail
+                resultant = findResultant(knots_vec.at(knot), knots_vec.at(knot+1));
+
+                // if vec between head & tail > 1 .. need to change tail position
+                if (resultant > 1.5){
+                    x_dif = knots_vec.at(knot)[0] - knots_vec.at(knot+1)[0]; // x&y vec between points 
+                    y_dif = knots_vec.at(knot)[1] - knots_vec.at(knot+1)[1];
+                
+                // if diagonal -> search for move to get it within 1 diagonal adjacent block from head
+                if (std::abs(x_dif) >= 1 && std::abs(y_dif) >= 1){
+                    //if (knot == 5) std::clog << "found match new diagonal.\n";
+                    for (const auto& xyd : xy_displacements){
+                        if (findResultant(knots_vec.at(knot), knots_vec.at(knot+1), xyd) <= 1.5){
+                            knots_vec.at(knot+1)[0] += xyd.first;
+                            knots_vec.at(knot+1)[1] += xyd.second;
+                            break;
+                        }
+                    }
+                    // x OR y vec == 0 -> whatever != 0, apply same move as applied to head
+                } else {
+                    if (std::abs(x_dif) > 0){
+                        knots_vec.at(knot+1)[0] += (x_dif > 0) ? 1 : -1;
+                    } else{
+                        knots_vec.at(knot+1)[1] += (y_dif > 0) ? 1 : -1;
+                    }
+                }
+                
+                // after changing tail, try to insert new tail xy coord into map
+                // currently only registers 1 move for tail 9 which is incorrect
+                if (knot == 0) knot1_visited_map.insert({{knots_vec.at(knot+1)[0],knots_vec.at(knot+1)[1]},true}); 
+                if (knot == 8){
+                    knot9_visited_map.insert({{knots_vec.at(knot+1)[0],knots_vec.at(knot+1)[1]}, true});
+                    /*
+                    std::clog << "Head coord: {" << knots_vec.at(knot)[0] << ',' << knots_vec.at(knot)[1] 
+                        << "}\n";
+                    std::clog << "Tail coord: {" << knots_vec.at(knot+1)[0] << ',' << knots_vec.at(knot+1)[1] 
+                        << "}\n";
+                    */
+                }
+                }
             }
-            //std::clog << "Tail coord: {" << tail[0] << ',' << tail[1] << "}" << '\n';
         }
     }
-    return visited_map.size();
+    return std::pair<size_t, size_t> {knot1_visited_map.size(), knot9_visited_map.size()};
 }
 
 #endif
