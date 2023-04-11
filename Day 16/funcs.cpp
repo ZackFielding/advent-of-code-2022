faf84f0cfae7154d08f4ee882ad296596b238c6e
zack <zackfielding@gmail.com>
2023-04-10 20:57:13 -0700

    working file parse & graph ctor

--- Day 16/funcs.cpp
+++ Day 16/funcs.cpp
@@ -1,54 +1,80 @@
 #include "funcs.hpp"
 
-/*
-Node class -> there are going to be two cases when instantiating a node 
-(1) the current node being created has not been created yet (1st ctor)
-(2) the current node has already been created as it was an adjacent of earlier node(s) (2nd ctor)
-*/
-
-/*
-TODO => DOES NOT CURRENTLY COMPILE
-[] need to change graph to reference node* alloc on heap
-*/
-
 node::node(std::string name) : valve_name {name}, flow_rate {}, adj_node_map {}{}
 
+// handles dirty work of creating new nodes if they don't already exist
+node* graph::__add_single_node__(std::string str){
+    auto check = this -> graph_map.find(str);
+    node* node_to_ret = nullptr;
+    if ( check == graph_map.end() ){
+       node_to_ret = new node {str}; 
+       graph_map.emplace(str, node_to_ret);
+    } else {
+        node_to_ret = check -> second;
+    }
+    return node_to_ret;
+}
+
 void graph::add_node (std::string vstr, const int32_t fr){
-    // try_emplace the current valve
+
     auto v = vstr.substr(0, 2);
-    const auto [iter, _] = this -> graph_map.try_emplace(v, node{v});
-    // set nodes flow rate
-    iter -> second.flow_rate = fr;
+    node* cnode = this -> __add_single_node__(v);
+    cnode -> flow_rate = fr;
 
     // set nodes adjacents
+    node* anode = nullptr;
     for (szt i {2}; i < vstr.size(); i += 2){
         // adjacent valve string
         std::string adj_str { vstr.substr(i, 2) };
         // try_emplace adjacent valve into map (no need to check if exists as that will take atleast >=2 calls opposed to 1 here)
-        const auto [adj_iter, _] = this -> graph_map.try_emplace( adj_str, node{adj_str} );
+        anode = this -> __add_single_node__(adj_str);
         // emplace current valves map with adjacent
-        iter -> second -> adj_node_map.try_emplace(adj_iter->first, adj_iter->second);
+        cnode -> adj_node_map.emplace(adj_str, anode);
+    }
+}
+
+/*
+- currently ONLY print_nodes() utilize __iter_nodes which recursively calls __rec_inner 
+    but set up with function args for future transform member functions
+*/
+/* ------------------------------------------------*/
+void graph::__rec_inner(miter citer, const miter eiter, std::function<void(miter)> iter_func){
+    if (citer == eiter) return;
+    iter_func(citer);
+    this -> __rec_inner(++citer, eiter, iter_func);
+}
+
+void graph::__iter_nodes__(std::function<void(miter)> iter_func, std::function<void(node*)> node_func){
+    for (const auto& n : graph_map){
+        this -> __rec_inner(n.second->adj_node_map.begin(), n.second->adj_node_map.end(), iter_func);
+        node_func(n.second);
     }
 }
 
 void graph::print_nodes(){
-
-    using mciter = sn_map::const_iterator;
-    void r_print_adj_nodes (mciter citer, mciter eiter){
-        if (iter == eiter) return;
-        std::clog << iter -> valve_name << ' ';
-        r_print_adj_nodes(++iter, eiter);
-    };
-
-    for (const auto n : graph_map){
-        std::cout << n.first << "'s adjacent nodes: ";
-        r_print_adj_nodes(n.second.adj_node_map.cbegin(), n.second.adj_node_map.cend());
-    }
+    auto _l1 = [](miter iter){ std::cout << iter -> first << ' '; };
+    auto _l2 = [](node* cnode) { std::cout << "... are adj to " << cnode -> valve_name << '\n'; };
+    this -> __iter_nodes__(_l1, _l2);
 }
 
-void read_file_create_graphs (std::ifstream& file, graph* g){
+graph::~graph(){
+    std::for_each(this -> graph_map.begin(), this -> graph_map.end(), [](auto& p) { delete p.second; });
+}
+/* --------------------------------------------- */
+std::string get_line_as_string (std::ifstream& file){
+    char c {};
+    std::string str_to_ret {};
+    while (file.get(c) && c != '\n') str_to_ret += c;
+    return str_to_ret;
+}
+
+void read_file_create_graph(std::ifstream& file, graph* g){
     std::string str;
-    while ( file >> str ){
+    while (1){
+        // get line as std string =>  if nothing extracted, break
+        str = get_line_as_string(file);
+        if (str == "") break;
+
         // rec get ALL valve names in current string
         std::string vstr {};
         vstr = r_ss(vstr, ++str.cbegin(), str.cend(), 65, 90, [](char){return false; });
--- Day 16/funcs.hpp
+++ Day 16/funcs.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <unordered_map>
 #include <functional>
+#include <algorithm>
 
 // fwd decs for using statements
 class node;
@@ -14,11 +15,12 @@ using szt = std::size_t;
 using strv_pos = std::pair < std::string, szt > ;
 using sn_map = std::unordered_map<std::string, node*>;
 using strciter = std::string::const_iterator;
+using miter = sn_map::iterator;
 
 class node {
   std::string valve_name;
   int32_t flow_rate;
-  std::unordered_map<std::string, node*> adj_node_map;
+  sn_map adj_node_map;
   node (std::string);
   node(std::string, const int32_t);
   friend graph;
@@ -29,8 +31,14 @@ struct graph {
   graph(): graph_map {} {}
   void add_node(std::string, const int32_t);
   void print_nodes();
+  ~graph();
+private:
+  node* __add_single_node__(std::string);
+  void __iter_nodes__(std::function<void(miter)>, std::function<void(node*)> node_func);
+  void __rec_inner(miter, const miter, std::function<void(miter)>);
 };
 
+std::string get_line_as_string (std::ifstream&);
 void read_file_create_graph (std::ifstream&, graph*);
 std::string r_ss (std::string, strciter, const strciter,
                     const int, const int, std::function<bool(char c)>);
